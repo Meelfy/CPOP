aa79c5bc2d8ff66700bae73592c65deb72d2c4b8
MajorChina <535370561@qq.com>
2016-10-27 16:28:54 +0800

    reshape pic sst PC

--- 3-EOF/EOF_SST_Space.m
+++ 3-EOF/EOF_SST_Space.m
@@ -38,7 +38,7 @@ function EOF_SST_Space = EOF_SST_Space()
         plot(1:m, EOF_SST_Space(:, i));
         axis([1 m min(EOF_SST_Space(:, i)) max(EOF_SST_Space(:, i))])
         set(gca,'xtick',1:60:m,'xticklabel',1981:5:2016);
-        title(['SST Principal component ', num2str(i)])
+        title(['SST Principal component ', num2str(i)], 'fontsize', 16)
         xlabel('Time serises - Year');        
         set(gcf, 'position', [0 0 1200 800]);
         set(gcf, 'color', 'w')
